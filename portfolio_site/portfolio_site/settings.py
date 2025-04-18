27e8302b7ebdd92bb2c8a26840afa10e79dc78fd
Andrii Balandin <andriibaland@gmail.com>
2025-04-18 20:09:02 +0300

    apps and base settings

--- portfolio_site/portfolio_site/settings.py
+++ portfolio_site/portfolio_site/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop',
+    'blog',
+    'cart',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -52,10 +56,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'portfolio_site.urls'
 
+TEMPLATES_DIRS = os.path.join(BASE_DIR, "templates")
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIRS],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
