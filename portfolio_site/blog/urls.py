da2d3b7052674118c599eea3f0233171e573714c
Andrii Balandin <andriibaland@gmail.com>
2025-05-01 10:26:40 +0300

    creating post

--- portfolio_site/blog/urls.py
+++ portfolio_site/blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView
 
 app_name = 'blog'
 urlpatterns = [
     path('', PostListView.as_view(), name='posts'),
+    path('post-create/', PostCreateView.as_view(), name='post-create'),
     path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
+
 ]
--- portfolio_site/blog/views.py
+++ portfolio_site/blog/views.py
@@ -1,11 +1,28 @@
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
+from django.utils.text import slugify
 
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/post_create.html'
+    context_object_name = 'task'
+    success_url = reverse_lazy('blog:posts')
+    fields = ['title', 'content', 'category', 'tags']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.slug = slugify(form.instance.title)
+        return super(PostCreateView, self).form_valid(form)
+
 
 
 class PostDetailView(DetailView):
